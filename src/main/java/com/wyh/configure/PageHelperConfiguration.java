a5a9f34e19ac88e5f0ac417b1a0b4454426b7043
Wangyaohang <204016200@qq.com>
2021-03-09 15:37:40 +0800

    分页插件配置

---
+++ src/main/java/com/wyh/configure/PageHelperConfiguration.java
@@ -0,0 +1,21 @@
+package com.wyh.configure;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.pagehelper.PageHelper;
+@Configuration
+public class PageHelperConfiguration {
+	@Bean
+	public PageHelper pageHelper() {
+	    PageHelper pageHelper = new PageHelper();
+	    Properties p = new Properties();
+	    p.setProperty("offsetAsPageNum", "true");
+	    p.setProperty("rowBoundsWithCount", "true");
+	    p.setProperty("reasonable", "true");
+	    pageHelper.setProperties(p);
+	    return pageHelper;
+	}
+}
